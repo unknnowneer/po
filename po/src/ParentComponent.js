b1d10b81dde5e2cc35d1fcfd0bb9b404a9b6cfea
unknnowneer <alina.alina-maakrova2016@yande.ru>
2023-10-17 23:56:39 +0700

    first

--- po/src/ParentComponent.js
+++ po/src/ParentComponent.js
@@ -1,55 +1,108 @@
-import React, { useState } from 'react';
-import MapWithInfoBlock from './MapWithInfoBlock';
+import React, { useState, useEffect }from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+const position = [55.030199, 82.920430];
 
-function CalendarBlock() {
-  const currentDate = new Date();
 
-  // Функция для форматирования даты в "DD.MM.YYYY"
-  const formatDate = (date) => {
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const year = date.getFullYear().toString();
-    return `${day}.${month}.${year}`;
-  };
-
-  const [selectedDate, setSelectedDate] = useState(formatDate(currentDate));
-
+const TextOverlay = ({ text, onChange }) => {
   return (
-    <div>
+    <div
+      style={{
+        position: 'absolute',
+        top: '100px',
+        left: '10px',
+        zIndex: '1000',
+        background: '#2196F3',
+        color: 'white',
+        borderRadius: '5px',
+        padding: '10px',
+        fontFamily: 'Arial, sans-serif',
+      }}
+    >
       <input
         type="text"
-        value={selectedDate}
-        onChange={(e) => setSelectedDate(e.target.value)}
+        value={text}
+        onChange={(e) => onChange(e.target.value)}
+        style={{
+          border: 'none',
+          background: 'white',
+          color: '#2196F3',
+          padding: '5px',
+          borderRadius: '3px',
+          fontSize: '14px',
+        }}
       />
     </div>
   );
-}
+};
+const CalendarOverlay = ({ date, onChange }) => {
+  return (
+    <div
+    style={{
+      position: 'absolute',
+      top: '10px',
+      left: '10px',
+      zIndex: '1000',
+      background: '#2196F3',
+      color: 'white',
+      borderRadius: '5px',
+      padding: '10px',
+      fontFamily: 'Arial, sans-serif',
+    }}
+  >
+    {/* Ваша разметка календаря */}
+    <input
+      type="date"
+      value={date}
+      onChange={(e) => onChange(e.target.value)}
+      style={{
+        border: 'none',
+        background: 'white',
+        color: '#2196F3',
+        padding: '5px',
+        borderRadius: '3px',
+        fontSize: '14px',
+      }}
+      />
+    </div>
+  );
+};
 
 const ParentComponent = () => {
-  const [selectedDate, setSelectedDate] = useState(getCurrentDate());
+  const [text, setText] = useState('');
+  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]);
 
-  function getCurrentDate() {
-    const date = new Date();
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const year = date.getFullYear();
-    return day + month + year;
-  }
+  const handleTextChange = (text) => {
+    setText(text);
+  };
 
-  function handleDateChange(newDate) {
-    setSelectedDate(newDate);
-  }
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  useEffect(() => {
+    setSelectedDate(new Date().toISOString().split('T')[0]);
+  }, []);
 
   return (
     <div>
-      <div className="map-wrapper">
-        <MapWithInfoBlock selectedDate={selectedDate} />
-      </div>
-      <div className="calendar-block">
-        <h3>Выберите дату:</h3>
-        <CalendarBlock />
-        <button onClick={() => handleDateChange('New Date')}>Изменить дату</button>
-      </div>
+      
+      <MapContainer center={position} zoom={13} style={{ height: '800px', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+      />
+      <Marker position={position}>
+          <TextOverlay text={text} onChange={handleTextChange} />
+        </Marker>
+       <Marker position={position}>
+        <CalendarOverlay date={selectedDate} onChange={handleDateChange} />
+      </Marker>
+      
+       <Marker position={position}>
+          <Popup>{text}</Popup>
+        </Marker>
+    </MapContainer>
     </div>
   );
 };
